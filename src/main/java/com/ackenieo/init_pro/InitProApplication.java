cff6fe5051c117d4d841fa2b098955cfddfdc140
Ackenieo <m13435714563@163.com>
2026-06-13 11:42:54 +0800

    chore: update project config, entry point, docs and init SQL

--- src/main/java/com/ackenieo/init_pro/InitProApplication.java
+++ src/main/java/com/ackenieo/init_pro/InitProApplication.java
@@ -3,9 +3,12 @@ package com.ackenieo.init_pro;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@MapperScan("com.ackenieo.init_pro.infrastructure.mapper")
+@MapperScan(basePackages = {"com.ackenieo.init_pro.user", "com.ackenieo.init_pro.conversation", "com.ackenieo.init_pro.evaluation", "com.ackenieo.init_pro.realtime"},
+            annotationClass = org.apache.ibatis.annotations.Mapper.class)
+@EnableAsync
 public class InitProApplication {
 
 	public static void main(String[] args) {
